2564ee3db6f0c14e1d755bd71a0095005a74dfa6
Ankit Dubey <ankit19933011@gmail.com>
2023-03-10 00:54:31 +0545

    Arithmetic application on java practice

---
+++ src/Arithmetic.java
@@ -0,0 +1,21 @@
+import java.awt.desktop.SystemEventListener;
+
+public class Arithmetic {
+    public static void main(String[] args){
+        int a;
+        int b;
+
+        a = 10;
+        b = 4;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("a + b = " + (a+b));
+        System.out.println("a - b = " + (a-b));
+        System.out.println("a * b = " + (a*b));
+        System.out.println("a / b = " + (a/b));
+        System.out.println("a % b = " + (a % b));
+
+
+    }
+}
