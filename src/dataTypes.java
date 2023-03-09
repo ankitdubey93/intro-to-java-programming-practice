cb1eeed8a4908cf510ad0c59bf3cf2604bafd9df
Ankit Dubey <ankit19933011@gmail.com>
2023-03-10 00:45:52 +0545

    Data types

---
+++ src/dataTypes.java
@@ -0,0 +1,18 @@
+public class dataTypes {
+    public static void main(String[] args) {
+        int i;
+        double d;
+        String s;
+        boolean b;
+
+        i = 55;
+        d = 50.5;
+        s = "Ankit";
+        b = false;
+
+        System.out.println("Integer i =" + i);
+        System.out.println("double d =" + d);
+        System.out.println("String s =" + s);
+        System.out.println("Boolean b =" + b);
+    }
+}
