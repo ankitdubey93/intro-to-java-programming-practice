875ca0fb4b460b317ebb2f3e224353e58c26c27c
Ankit Dubey <ankit19933011@gmail.com>
2023-03-10 21:49:36 +0545

    New method for the integer overflow porblem, changed the int to Bigin

--- src/Age.java
+++ src/Age.java
@@ -1,26 +1,23 @@
 //This program asks the age in years, converts it and displays Age in months, days, hours and seconds.
 
+import java.math.BigInteger;
 import java.util.*;
 public class Age {
     private static Scanner input = new Scanner(System.in);
     public static void main(String[] args){
-        int aiy; //defining an integer variable "age in years"
-        int aim; //defining an integer variable "age in months"
-        int aid; //defining an integer variable "age in days"
-        int aih; //defining an integer variable "age in hours"
-        int ais; //defining an integer variable "age in seconds"
+        int ageinyears; //defining an integer variable "age in years"
 
         System.out.println("What is your age in years?");
-        aiy = input.nextInt();
+        ageinyears = input.nextInt();
 
-        aim = (aiy * 12);
-        aid = (aim * 365);
-        aih = (aid * 24);
-        ais = (aih * 60 * 60);
+        int ageinmonths = (ageinyears * 12); //defining an integer variable "age in months"
+        int ageindays = (ageinmonths * 365);
+        int ageinhours = (ageindays * 24);
+        BigInteger ageinseconds = BigInteger.valueOf(ageinhours).multiply(BigInteger.valueOf(3600)); //defining an integer variable "age in seconds"
 
-        System.out.println("Your age in months is " + aim);
-        System.out.println("Your age in days is " + aid);
-        System.out.println("Your age in hours is " + aih);
-        System.out.println("Your age in seconds is " + ais);
+        System.out.println("Your age in months is " + ageinmonths);
+        System.out.println("Your age in days is " + ageindays);
+        System.out.println("Your age in hours is " + ageinhours);
+        System.out.println("Your age in seconds is " + ageinseconds );
     }
 }
