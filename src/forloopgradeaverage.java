6abc2c0f56143e02c1f1845d8dff77b763c8689b
Ankit Dubey <ankit19933011@gmail.com>
2023-03-22 03:05:22 +0545

    for loop tested on previous program of grade average

---
+++ src/forloopgradeaverage.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class forloopgradeaverage {
+    public static Scanner input = new Scanner(System.in);
+
+    public static void main(String [] args) {
+        try {
+            int subjects = getValue("No of subjects", false);
+            int scores = getScores(subjects);
+            double average = scores/subjects;
+            System.out.println("The average of your scores are " + average);
+        } catch(Exception error) {
+            System.out.println(error.getMessage());
+            System.exit(0);
+        }
+
+    }
+
+    public static int getValue(String name, boolean allowZero) {
+        System.out.println("Enter " + name);
+        int value = input.nextInt();
+
+        int minValue = allowZero ? 0 : 1;
+
+
+        while(value < minValue) {
+            System.out.println(value + " is not a valid input. Please enter a number greater than or equal to " + minValue);
+            value = input.nextInt();
+        }
+
+        return value;
+    }
+
+    public static int getScores(int subject) {
+        int score = 0;
+
+        for (int count = 1;count <=subject; count++)
+            score = score + getValue(" score " + count, true);
+
+        return score;
+        }
+
+    }
+
+
